a423d913b5b161e7262c787836d5b2cea1b5950a
Saumil Jain <156582131+jsaumil@users.noreply.github.com>
2025-07-18 17:15:26 +0530

    Updated files

--- Runnables/runnable_lambda.py
+++ Runnables/runnable_lambda.py
@@ -2,6 +2,6 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
-from langchain.schema.runnable import RunnableParallel, RunnableSequence, RunnablePassthrough
+from langchain.schema.runnable import RunnableLambda
 
 load_dotenv()
