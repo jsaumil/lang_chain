28c49fa66ecd3be0c370d25e656cdc24b0f64866
Saumil Jain <156582131+jsaumil@users.noreply.github.com>
2025-07-18 17:27:34 +0530

    Updated files

--- Runnables/runnable_branch.py
+++ Runnables/runnable_branch.py
@@ -2,6 +2,11 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
-from langchain.schema.runnable import RunnableSequence, RunnableBranch, RunnablePassthrough, RunnableParallel
+from langchain.schema.runnable import RunnableSequence, RunnableBranch, RunnablePassthrough, RunnableParallel, RunnableLambda
 
 load_dotenv()
+
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
