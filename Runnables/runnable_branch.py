55ec24b0cf41a8c54632d2eaa3ef1fd96d147298
Saumil Jain <156582131+jsaumil@users.noreply.github.com>
2025-07-18 17:27:00 +0530

    Updated files

--- Runnables/runnable_branch.py
+++ Runnables/runnable_branch.py
@@ -2,5 +2,6 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
-from langchain.schema.runnable import RunnableSequence
+from langchain.schema.runnable import RunnableSequence, RunnableBranch, RunnablePassthrough, RunnableParallel
 
+load_dotenv()
